6bceea914f0b498f9a32cb00e7275cfb4c008693
Hùng Nguyễn Quốc <imbayonline@gmail.com>
2021-04-06 15:27:38 +0700

    new changes

--- server/server.js
+++ server/server.js
@@ -2,40 +2,40 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
-const mongoose = require("mongoose");
-const passport = require("passport");
+// const mongoose = require("mongoose");
+// const passport = require("passport");
 const os = require("os");
 require("dotenv").config();
-require("./passport.js");
+// require("./passport.js");
 
-const accounts = require("./routes/accounts");
-const activities = require("./routes/activities");
-mongoose.connect(
-  os.platform() === "win32"
-    ? `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-4veva.gcp.mongodb.net/chatops?retryWrites=true&w=majority`
-    : `mongodb://db:27017/chatops`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (error) => {
-    if (error) console.log(error);
-    console.log("Connect to database");
-  }
-);
+// const accounts = require("./routes/accounts");
+// const activities = require("./routes/activities");
+// mongoose.connect(
+//   os.platform() === "win32"
+//     ? `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-4veva.gcp.mongodb.net/chatops?retryWrites=true&w=majority`
+//     : `mongodb://db:27017/chatops`,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (error) => {
+//     if (error) console.log(error);
+//     console.log("Connect to database");
+//   }
+// );
 
 app.use(cors());
 app.use("/", express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
-app.get("/",(req,res) => {
+app.get("/api",(req,res) => {
     res.status(200).json({message: "Test"});
 });
 
-app.use("/accounts", accounts);
-app.use("/activities", activities);
+// app.use("/accounts", accounts);
+// app.use("/activities", activities);
 app.listen(port, () => {
   console.log(`Listening on port ${port} on ${os.platform()}`);
 });
